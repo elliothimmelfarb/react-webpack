e2377646c0050bfe6c4374b0f1dc70b8d55df65b
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-07-28 15:26:20 -0700

    up and runnig

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello from main.js</h1>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        excludes: ['node_modules'],
+      },
+    ],
+  },
+};
